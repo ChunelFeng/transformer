caabd8b2b3daad47c5b157d17acc40d2d061d13b
Yevgnen Koh <wherejoystarts@gmail.com>
2017-09-30 15:12:12 +0800

    Initialization.

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+setup(
+    name='transfomer',
+    description='Attention is all you need',
+    version='0.0.1',
+    author='Yevgnen Koh',
+    author_email='wherejoystarts@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    zip_safe=False)
